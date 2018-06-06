cb99f64c0d27e6359cac22aa8b871ae7fcbc316d
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-06 16:48:34 +0200

    add exercice 2 ex8

---
+++ Exercice 2/ex8/script.js
@@ -0,0 +1,12 @@
+//Exercice 8
+
+/*
+Créer un bouton. Au click de ce bouton, lancer la fonction jourDeLaSemaine().
+Cette fonction détermine le jour de la semaine et affiche selon le cas dimanche, lundi, mardi... etc. jusqu'à samedi.
+*/
+
+function jourDeLaSemaine(){
+
+}
+
+var date = new Date();
