f4d33bab782e0ca59d8a00cd3596d7011da6134b
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-06 09:40:57 +0200

    exercice 2 ex3 finish and add ex4

---
+++ Exercice 2/ex4/script.js
@@ -0,0 +1,17 @@
+//Exercice 3
+
+var a = 3, b = 2;
+
+function triple(x) {
+
+  var triple = x*3;
+
+  return triple;
+
+}
+
+function afficher(){
+
+  alert("le triple de "+a+" est: "+triple(a));
+  alert("le triple de "+b+" est: "+triple(b));
+}
