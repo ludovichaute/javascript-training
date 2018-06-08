f8be6f8f3ab9600511f37a33d4c00d06e00e0b2e
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-08 10:10:11 +0200

    exercice2 ex9 finish

---
+++ Exercice 2/ex9/script.js
@@ -0,0 +1,17 @@
+//Exercice 9 - utilisation de la boucle while
+
+/*
+Créer un bouton. Au click de ce bouton, lancer la fonction testWhile.
+Cette fonction demande à l'utilisateur de saisir une chaîne de caractères contenant la lettre p,
+et le redemande tant que l'utilisateur ne l'a pas fait.
+Il affiche ensuite la chaîne saisie entre guillemets.
+*/
+
+
+
+function testWhile() {
+  do {
+    var lettre = prompt("entrer une lettre: ")
+  } while (lettre !== "p");
+  alert("\""+lettre+'"');
+}
