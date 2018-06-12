6e0880ce36231a13ac1a9372af85538e2224a0e6
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-12 16:33:23 +0200

    serie3 ex

--- serie3/ex3/script.js
+++ serie3/ex3/script.js
@@ -1,4 +1,10 @@
 //exercice 3
+// Dans le fichier HTML fourni, afficher une boite de dialogue avec la valeur du champ dès que l'on appuie sur le clavier.
 
+document.getElementById("nom").addEventListener("keypress", myFunction);
 
+var x = document.getElementById("myText").value;
 
+function myFunction() {
+    document.getElementById("nom").style.backgroundColor = "red";
+}
