f0a7f9e98100ac293e8411f0729ff84f36b21863
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-20 09:06:48 +0200

    serie4 ex3 finish

--- serie4/ex3/script.js
+++ serie4/ex3/script.js
@@ -1,22 +1,23 @@
 /*
 Exercice 3
 Dans le fichier HTML fourni, changer la couleur du texte en fonction du bouton choisi.
-
 */
-
 document.getElementsByClassName("color")[0].addEventListener("click", green);
 document.getElementsByClassName("color")[1].addEventListener("click", red);
 document.getElementsByClassName("color")[2].addEventListener("click", blue);
 
 function green(){
-    console.log("green")
+    document.getElementsByTagName("p")[0].classList.remove("blue");
+    document.getElementsByTagName("p")[0].classList.remove("red");
     document.getElementsByTagName("p")[0].classList.add("green");
 }
 function red(){
-    console.log("red")
+    document.getElementsByTagName("p")[0].classList.remove("green");
+    document.getElementsByTagName("p")[0].classList.remove("blue");
     document.getElementsByTagName("p")[0].classList.add("red");
 }
 function blue(){
-    console.log("blue")
+    document.getElementsByTagName("p")[0].classList.remove("red");
+    document.getElementsByTagName("p")[0].classList.remove("green");
     document.getElementsByTagName("p")[0].classList.add("blue");
 }
