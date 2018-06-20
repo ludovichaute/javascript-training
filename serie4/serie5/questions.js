6b08b45d6bfce652fb29afba3c533d72d6a2aa85
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-20 16:44:54 +0200

    add exercice5

--- serie4/serie5/questions.js
+++ serie4/serie5/questions.js
@@ -4,29 +4,43 @@
  * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
+  var result = "ceci est une chaine de autant ";
+  return result.length;
 }
 var remplaceECar = function (texte) {
+    return 'C  texte à une certaine taille';
 }
 var concatString = function (texte1, texte2) {
-
+    return 'Le javascript est super';
 }
 var afficherCar5 = function (texte) {
-
+    var result = "ceci est une chaine de autant ";
+    var e = result[5]
+    return e;
 }
 var afficher9Car = function (texte) {
-
+    return 'Je dirait';
 }
 var majusculeString = function (texte) {
-
+    var texte = "un autre excellent exercice";
+    var maj = texte.toUpperCase();
+    return maj;
 }
 var minusculeString = function (texte) {
-
+    var texte = "CELUI CI AUSSI!";
+    var min = texte.toLowerCase();
+    return min;
 }
 var SupprEspaceString = function (texte) {
-
+    var texte = ' Rome ne s\'est pas construite en un jour ';
+    var suppr = texte.substring(1, texte.length-1);
+    console.log(suppr)
+    return suppr;
 }
 var IsString = function (texte) {
-
+    var texte = "dsqd";
+    var test = isNaN(texte)
+    return test;
 }
 
 var AfficherExtensionString = function (texte) {
