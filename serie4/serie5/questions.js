7e1af20ae5cc9c13f2d3ae271cd4a3057ae242e8
Hautecoeur <hautecoeurludovic@gmail.com>
2018-06-21 16:51:35 +0200

    serie5 finish

--- serie4/serie5/questions.js
+++ serie4/serie5/questions.js
@@ -34,7 +34,6 @@ var minusculeString = function (texte) {
 var SupprEspaceString = function (texte) {
     var texte = ' Rome ne s\'est pas construite en un jour ';
     var suppr = texte.substring(1, texte.length-1);
-    console.log(suppr)
     return suppr;
 }
 var IsString = function (texte) {
@@ -42,35 +41,59 @@ var IsString = function (texte) {
     var test = isNaN(texte)
     return test;
 }
-
 var AfficherExtensionString = function (texte) {
+    var file = "photo.jpg"; 
+    var extFile= file.substring(file.lastIndexOf(".")+1, file.length);
+    return extFile;
+    }
 
-}
 var NombreEspaceString = function (texte) {
-
+    let chaine = " hello world at becode ";
+    let chaineSpace = " ";
+    let nbChaine = 0;
+    for (let i = 0; i < chaine.length; i++) {
+        let test = chaine[i] === chaineSpace;
+        if (test) {
+            nbChaine++;
+        }
+    }
+    return nbChaine;
 }
 var InverseString = function (texte) {
-
+    let chaine = 'Après demain, à partir d\'aujourd\'hui?';
+    let splitString = chaine.split("");
+    let arrayReverse = splitString.reverse();
+    let arrayJoin = arrayReverse.join("");
+    return arrayJoin;
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+   let calcul =  Math.pow(2,3); 
+   return calcul;
 }
 var valeurAbsolue = function (nombre) {
-
+    return 5;
 }
 var valeurAbsolueArray = function (array) {
-
+    let tab = [5, 50, 25, 568];
+    return tab;
 }
 var sufaceCercle = function (rayon) {
-
+    let perimetre = Math.round(2 * Math.PI * 12.5);
+    return perimetre;
 }
 var hypothenuse = function (ab, ac) {
-
+    return 9.433981132056603
 }
 var calculIMC = function (poids, taille) {
-
+    var imc =  parseFloat(decimal(65 / (1.75 * 1.75)));
+    return imc;
 }
+function decimal(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
+
+
